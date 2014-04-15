1b540270d6289831e780af16e627480126c0556b
Nutsi <corentin.rivot@epitech.eu>
2014-04-15 15:18:24 +0000

    syscall, modify syscall pslot

--- linux/arch/x86/kernel/sys_pslot.c
+++ linux/arch/x86/kernel/sys_pslot.c
@@ -2,15 +2,40 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
+#include <linux/list.h>
 
-asmlinkage long	sys_pslot(int a, int b)
+asmlinkage long	sys_pslot(int control, void __user *buf)
 {
 	#ifdef CONFIG_PROC_PSLOT
-
-	printk(KERN_INFO "pslot syscall is called\n");
-	#else
-		printk(KERN_WARN "pslot called failed CONFIG_PROC_PSLOT=n");
-		return -1;
-	#endif
+	/* When monitoring must be started, we must reset the linked list
+	 * to avoid bad result
+	 */
+	if (control == 0)
+	{
+		pslot_state = PSLOT_STATE_START;
+		struct list_head *l, *q;
+		list_for_each_safe(l, q, &init_pslot_list)
+		{
+			struct pslot_linked_entity *entry = list_entry(l, struct pslot_linked_entity, list);
+			list_del(l);
+			kfree(entry);
+		}
+		LIST_HEAD(init_pslot_list);
+		goto pslot_normal_exit;
+	}
+	else if (control == -1)
+		pslot_state = PSLOT_STATE_STOP;
+	else if (control > 0)
+	{
+		copy_to_user(buf, &control, sizeof(int));
+		//(int)(*buf) = control;
+	}
 	return 0;
+	#endif
+
+	printk(KERN_INFO "pslot called failed CONFIG_PROC_PSLOT=n");
+	return -1;
+
+	pslot_normal_exit:
+		return 0;
 }
